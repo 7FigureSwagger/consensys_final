fc9c261d3f918daba804c709ec941a2f4787f50b
Ali Swistak <aswistak@linkpoint.ca>
2020-12-29 13:43:18 -0800

    Receive fallback function, withdraw function modified.

--- contracts/DumpEth.sol
+++ contracts/DumpEth.sol
@@ -16,13 +16,24 @@ contract DumpEth {
 
     // Events to emit for user deposit to contract and withdrawal
     event LogDepositMade(address accountAddress, uint256 amount);
-
     event LogWithdrawalMade(address accountAddress, uint256 amount);
+		
+		// Event to emit when ETH received without calldata
+		event Received(address sender, uint value);
+
 
     // Fallback function in case tx is sent without ether or calls function that does not exist
     fallback() external payable {
         revert();
     }
+
+		// Fallback 'receive' funciton to collect ETH sent with empty calldata
+		receive()
+				external
+				payable 
+		{
+				emit Received(msg.sender, msg.value);
+		}
 
     // Function to deposit to contract
     function deposit() public payable returns (uint256) {
@@ -36,13 +47,13 @@ contract DumpEth {
     }
 
     // Function to withdraw from contract
-    function withdraw() private returns (uint256) 
+    function withdraw(uint _amount) private returns (uint256) 
 		{
 				// Only allow the 'deployer' of the contract to withdraw
-				require(address(msg.sender) == owner);
+				require(address(msg.sender) == owner && balance[msg.sender] >= _amount);
         {
-						balance[msg.sender] = balance[msg.sender] -= msg.value;
-            emit LogWithdrawalMade(address(msg.sender), msg.value);
+						balance[msg.sender] = balance[msg.sender] -= _amount;
+            emit LogWithdrawalMade(address(msg.sender), _amount);
             return balance[msg.sender];
         }
     }
