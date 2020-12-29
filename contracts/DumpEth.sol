fda63043c258d2f5fda54fb53c1e198de86db573
Ali Swistak <aswistak@linkpoint.ca>
2020-12-29 15:12:05 -0800

    Circuit breaker design pattern added. Modifiers and functions to stop contract and do an emergency withdrawal.

--- contracts/DumpEth.sol
+++ contracts/DumpEth.sol
@@ -7,7 +7,26 @@ contract DumpEth {
     mapping(address => uint256) private balance;
 
     // Hold the owner of the contract in state
-    address public owner;
+    address private owner;
+
+		// State for circuit breaker
+		bool private stopped;
+
+		// Ensure caller is admin of contract
+		modifier isAdmin(){
+			require(msg.sender == owner);
+			_;
+		}
+
+		// Ensure contract is not stopped before proceding
+		modifier emergencyStop(){
+			require(!stopped);
+			_;
+		}
+		modifier onlyAfterStopped(){
+			require(stopped);
+			_;
+		}
 
     constructor() {
         // Set the owner to the creator of this contract
@@ -17,7 +36,7 @@ contract DumpEth {
     // Events to emit for user deposit to contract and withdrawal
     event LogDepositMade(address accountAddress, uint256 amount);
     event LogWithdrawalMade(address accountAddress, uint256 amount);
-		
+
 		// Event to emit when ETH received without calldata
 		event Received(address sender, uint value);
 
@@ -35,8 +54,13 @@ contract DumpEth {
 				emit Received(msg.sender, msg.value);
 		}
 
+		// Function for stopping contract 
+		function toggleActive() isAdmin public{
+			stopped = !stopped;
+		}
+
     // Function to deposit to contract
-    function deposit() public payable returns (uint256) {
+    function deposit() emergencyStop public payable returns (uint256) {
         // Only allow the 'deployer' of the contract to deposit
         require(address(msg.sender) == owner);
         {
@@ -47,7 +71,7 @@ contract DumpEth {
     }
 
     // Function to withdraw from contract
-    function withdraw(uint _amount) private returns (uint256) 
+    function withdraw(uint _amount) emergencyStop private returns (uint256) 
 		{
 				// Only allow the 'deployer' of the contract to withdraw
 				require(address(msg.sender) == owner && balance[msg.sender] >= _amount);
@@ -58,6 +82,16 @@ contract DumpEth {
         }
     }
 
+		// Emergency withdrawal, after circuit breaker flipped
+		function emergencyWithdrawal() onlyAfterStopped private
+		{
+			require(address(msg.sender) == owner && balance[msg.sender] > 0);
+			{
+					uint256 amt = balance[msg.sender];
+					balance[msg.sender] = balance[msg.sender] -= balance[msg.sender];
+					emit LogWithdrawalMade(address(msg.sender), amt);
+			}
+		}
     // Function to check balance of contract
     function getContractBalance() private view returns (uint256) {
         return address(this).balance;
